9a3c53ea0d372f1f41064f5c47a96ef482c246b3
MikhailKazaninWG <mishakazanin@gmail.com>
2022-11-07 23:06:28 +0300

    feat: add code documentation (SmoothButton, SignInField)

--- src/components/sign_in/SignInField/SignInField.js
+++ src/components/sign_in/SignInField/SignInField.js
@@ -1,5 +1,10 @@
 import "./SignInField.css"
 
+/** field used in sign in page
+ * @param {string} title - name of the field
+ * @param {string} additionalInfo - some information you want to add
+ * @param {string} type - type of the field
+ */
 const SignInField = (props) => {
     return (
         <div className={"sign-in-field"}>
@@ -9,7 +14,7 @@ const SignInField = (props) => {
             <div className={"additional-info"}>
                 {props.additionalInfo}
             </div>
-            <input type={props.type} id={props.id}/>
+            <input type={props.type}/>
         </div>
     )
 }
--- src/components/ui/SmoothButton/SmoothButton.js
+++ src/components/ui/SmoothButton/SmoothButton.js
@@ -1,8 +1,13 @@
 import './SmoothButton.css'
 
+/**
+ * Smoothly filling button
+ * @param {string} type - type of the button
+ * @param {string} title - text in the button
+ */
 const SmoothButton = (props) => {
     return (
-        <button className={"smooth-button"} type={props.type} id={props.id}>{props.title}</button>
+        <button className={"smooth-button"} type={props.type}>{props.title}</button>
     )
 }
 
